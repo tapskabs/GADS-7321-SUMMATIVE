803081bae9209a888d08e723405f19db56e3ebfe
AmberWinds <angie.22.ds@gmail.com>
2025-06-16 14:15:55 +0200

    CompletedMainMenu

---
+++ Assets/MainMenuObjects/MainMenuManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    MainMenuManager Instance;
+    public AudioMixer audioMixer;
+    public Canvas settingsMenu;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartNewGame()
+    {
+       // SceneManager.LoadScene();
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.gameObject.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu.gameObject.SetActive(false);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetFullscreen(bool isOn)
+    {
+        Screen.fullScreen = isOn;
+    }
+}
